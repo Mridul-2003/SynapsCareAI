21efe47135c9a25500dd71dbabff54fd09740776
Ajay <112794893+Ajay-308@users.noreply.github.com>
2026-04-03 19:31:41 +0530

    fixed eslint error

--- front/eslint.config.js
+++ front/eslint.config.js
@@ -1,4 +1,23 @@
 import js from "@eslint/js";
 import next from "eslint-config-next";
 
-export default [js.configs.recommended, next];
+export default [
+  js.configs.recommended,
+  ...next,
+  {
+    languageOptions: {
+      globals: {
+        React: "readonly", // React warnings band
+        NodeJS: "readonly", // NodeJS warnings band
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+      "react-hooks/purity": "off",
+      "react/no-unescaped-entities": "off",
+      "import/no-anonymous-default-export": "off", // eslint.config.js warning band
+    },
+  },
+];
